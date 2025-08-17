319a5b93b7fdb2d6921884765d2babfc5ace290b
Biokes <106444765+Biokes@users.noreply.github.com>
2025-08-17 11:41:38 +0100

    update

---
+++ contracts/multiSig.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {MultiSigDAO} from "./multiSigDao.sol";
+import {Test} from "forge-std/Test.sol";
+
+
+
+contract MultiSigDaoTest is Test {
+  MultiSigDAO dao;
+  function setup()public{
+    address user1 = address(1);
+    address user2 = address(2);
+    address user3 = address(3);
+    dao = new MultiSigDAO(user1, user2, user3);
+  }
+  function testsUserCanCreateProposals()public{
+    address user101 = address(101);
+    vm.startPrank(user101);
+    dao.createProposal("proposal 101 is not free");
+    assert(dao.allUserProposals(user101).length,1);
+    // assert();
+  }
+}
--- contracts/multiSigDao.sol
+++ contracts/multiSigDao.sol
@@ -9,16 +9,37 @@ contract MultiSigDAO {
         uint id;
         string name;
     }
-    uint counter = 10001;
+    // struct Vote{
+    //     uint voteCount;
+    //     bool isSupporting;
+    // }
+    uint private counter = 10001;
+    
+    // mapping(uint => mapping(address=>Vote)) votes;
     mapping(address => User) private users;
+    mapping(uint => Proposal) private approvedProposals;
+    mapping(uint => Proposal) private rejectedProposals;
 
     constructor(address admin1, address admin2, address admin3) {
+        require(admin1 != address(0) && admin2 != address(0) && admin3 != address(0),"Zero address not allowed");
+        require(admin1 != admin2, "admin1 and admin2 must be different");
+        require(admin1 != admin3, "admin1 and admin3 must be different");
+        require(admin2 != admin3, "admin2 and admin3 must be different");
         _admin1 = admin1;
         _admin2 = admin2;
         _admin3 = admin3;
     }
 
-    function addUser(string memory name, address userAddress) external {
+    modifier isNotExisitingAddress(address userAddress) {
+        User memory newUser = users[userAddress];
+        require(newUser.id == 0, "user already exist");
+        _;
+    }
+
+    function addUser(
+        string memory name,
+        address userAddress
+    ) external isNotExisitingAddress(userAddress) {
         User memory user = User(counter, name);
         users[userAddress] = user;
         counter++;
@@ -40,18 +61,6 @@ contract MultiSigDAO {
     }
     Proposal[] private allProposals;
     mapping(uint => Proposal) private proposals;
-
-    function createProposal(string memory description) external onlyAdmin {
-        counter += 1;
-        Proposal memory proposal;
-         proposal.status= Status.PENDING;
-         proposal.description= description;
-         proposal.id= counter;
-         proposal.user= msg.sender;
-        allProposals.push(proposal);
-        proposals[counter]= proposal;
-    }
-
     error UNAUTHORIZED(address);
 
     modifier onlyAdmin() {
@@ -65,7 +74,7 @@ contract MultiSigDAO {
     }
 
     modifier hasNotApprovedYet(uint proposalId) {
-        Proposal storage proposal = allProposals[proposalId];
+        Proposal memory proposal = allProposals[proposalId];
         for (uint i = 0; i < proposal.approvals.length; i++) {
             require(proposal.approvals[i] != msg.sender, "Already approved");
         }
@@ -73,21 +82,49 @@ contract MultiSigDAO {
     }
 
     modifier hasNotRejectedYet(uint proposalId) {
-        Proposal storage proposal = allProposals[proposalId];
+        Proposal memory proposal = allProposals[proposalId];
         for (uint i = 0; i < proposal.approvals.length; i++) {
             require(proposal.rejections[i] != msg.sender, "Already rejected");
         }
         _;
     }
-    mapping(uint => Proposal) private approvedProposals;
-    mapping(uint => Proposal) private rejectedProposals;
 
-    function approveProposals(uint proposalId) external onlyAdmin hasNotApprovedYet(proposalId) {
+    modifier isRejectedDS(uint proposalId) {
+        Proposal memory proposal = allProposals[proposalId];
+        for (uint i = 0; i < proposal.rejections.length; i++) {
+            require(
+                proposal.rejections[i] != msg.sender,
+                "Already Disapproved"
+            );
+        }
+        _;
+    }
+
+    modifier isApprovingDS(uint proposalId) {
+        Proposal memory proposal = allProposals[proposalId];
+        for (uint i = 0; i < proposal.approvals.length; i++) {
+            require(proposal.approvals[i] != msg.sender, "Already Disapproved");
+        }
+        _;
+    }
+
+    function createProposal(string memory description) external onlyAdmin {
+        counter += 1;
+        Proposal memory proposal;
+        proposal.status = Status.PENDING;
+        proposal.description = description;
+        proposal.id = counter;
+        proposal.user = msg.sender;
+        allProposals.push(proposal);
+        proposals[counter] = proposal;
+    }
+
+    function approveProposals(uint proposalId) external onlyAdmin hasNotApprovedYet(proposalId) isRejectedDS(proposalId) {
         for (uint i = 10000; i < allProposals.length; i++) {
             if (allProposals[i].id == proposalId) {
                 allProposals[i].approvals.push(msg.sender);
-                if(allProposals[i].approvals.length>=2){
-                    allProposals[i].status= Status.ACCEPTED;
+                if (allProposals[i].approvals.length >= 2) {
+                    allProposals[i].status = Status.ACCEPTED;
                     approvedProposals[proposalId] = allProposals[proposalId];
                 }
                 return;
@@ -95,24 +132,21 @@ contract MultiSigDAO {
         }
     }
 
-    function rejectProposals(uint proposalId) external onlyAdmin hasNotRejectedYet(proposalId) {
+    function rejectProposals(uint proposalId) external onlyAdmin hasNotRejectedYet(proposalId) isApprovingDS(proposalId) {
         for (uint i = 10000; i < allProposals.length; i++) {
             if (allProposals[i].id == proposalId) {
                 allProposals[i].approvals.push(msg.sender);
-                 if(allProposals[i].approvals.length>=2){
-                    allProposals[i].status= Status.REJECTED;
-                    rejectedProposals[proposalId]= allProposals[i];
+                if (allProposals[i].rejections.length >= 2) {
+                    allProposals[i].status = Status.REJECTED;
+                    rejectedProposals[proposalId] = allProposals[i];
                 }
                 return;
             }
         }
     }
-    
 
+    // function vote(uint proposalId, bool isFor) external {
+    //     require(votes[proposalId][msg.sender].voteCount!=1, "Already voted for");
+    //     votes[proposalId][msg.sender].isSupporting= isFor;
+    // }
 }
-
-// in a Passed Proposals list, and failed proposals should be stored in a Failed Proposals list.
-//Your task:
-//👉 Write a smart contract in Solidity that implements this DAO system.
-//👉 Include functions for joining the DAO, creating a proposal, and admin voting.
-//👉 Test your contract with different scenarios using hardha
